ebd7d34fc68149cced0834cb6d6ae6d566b68dfc
C-Train275 <calebandewwallis@gmail.com>
2026-08-20 14:47:22 +0930

    Fixed typo in config file

--- pynq/signal_detection/src/config.py
+++ pynq/signal_detection/src/config.py
@@ -8,7 +8,7 @@ class Config:
     fs_mult: int = 4
     bandwidth: float = 2e6
     f: float = 1090e6
-    sig_len: int = 120e-6 * fs
+    sig_len: int = 120e-6 * fs * fs_mult
     preamble_mask: npt.NDArray = np.array(
         [True,False,True,False,False,False,False,True,False,True,False,False,False,False,False,False],
         dtype=bool
